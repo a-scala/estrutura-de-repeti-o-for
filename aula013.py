eb8d6115f8ebf0a5f0420e2318ebea1dc90f8ba7
a-scala <alves.scala07@gmail.com>
2026-09-25 16:19:38 +0200

    aula013

--- aula013.py
+++ aula013.py
@@ -1,1 +1,22 @@
-print(aaaaaa)
+'''for c in range(6, -1, -1):
+    print(c)
+print('FIM')'''
+
+'''n =int(input('Digite um numero: '))
+for c in range(n, 0, -1):
+    print(c)
+print('FIM')'''
+
+'''i = int(input('Inicio: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')'''
+
+s = 0
+for c in range(0, 3):
+    n = int(input('Digite um valor: '))
+    s = s + n
+print(f'A soma dos valores foi {s}')
+
